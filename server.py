ea05141d55eb955672308066674a1dd9d8ac35b4
russell-stewart <rstewart16@kentdenver.org>
2016-02-25 09:02:11 -0700

    Change from node to webpy

---
+++ server.py
@@ -0,0 +1,36 @@
+import os
+import web
+
+render = web.template.render('website/')
+
+urls = (
+    '/' , 'index'
+)
+
+app = web.application(urls, globals(), True)
+
+class Post:
+    name = ""
+    text = ""
+    comments = []
+
+class Comment:
+    name = ""
+    text = ""
+
+class index:
+    def GET(self):
+        p = Post()
+        p.name = 'Russell'
+        p.text = 'Is this working?'
+        c = Comment()
+        c.name = 'Bob'
+        c.text = 'This should be a comment'
+        comments = [c]
+        p.comments = comments
+        posts = [p]
+        return render.index(posts)
+
+#main method
+if __name__ == '__main__':
+    app.run()

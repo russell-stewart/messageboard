4e4af8d24b254d4b17b68a14a735c965460fe0e1
russell-stewart <rstewart16@kentdenver.org>
2016-02-29 13:25:38 -0700

    Add object constructors for Post and Comment

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 import os
 import web
+import pymongo#note: you must first run this terminal command: sudo python -m pip install pymongo to use this package.
+#Database setup: Create a database named messageboard. Add a collection called posts.
+from pymongo import MongoClient
+client = MongoClient('localhost' , 27017)
+db = client.messageboard
+collection = db.posts
+
 
 render = web.template.render('website/')
 
@@ -9,33 +16,33 @@ urls = (
 
 app = web.application(urls, globals(), True)
 
+#a class for organizing posts
 class Post:
-    name = ""
-    text = ""
-    pic = ""
-    comments = []
-    myid = -1
+    def __init__(self , n , t , p , c , m):
+        self.name = n
+        self.text = t
+        self.pic = p
+        self.comments = c
+        self.myid = m
 
+#a class for organizing comments
 class Comment:
-    name = ""
-    text = ""
-    referenceid = -1
+    def __init__(self , n , t , r):
+        self.name = n
+        self.text = t
+        self.referenceid = r
 
+#the main website homepage
 class index:
+    #returns the website
     def GET(self):
-        p = Post()
-        p.name = 'Russell'
-        p.text = 'Is this working?'
-        p.pic = 'https://kentdenver.instructure.com/images/thumbnails/15304/ckhEcjfPPMuJeUIXGPMdg7QoyNC0c6Ptd5vdGKfe'
-        p.id = 1
-        c = Comment()
-        c.name = 'Bob'
-        c.text = 'This should be a comment'
-        c.referenceid = 1
+        c = Comment('Bob' , 'This should be a comment.' , 1)
         comments = [c]
-        p.comments = comments
+        p = Post('Russell' , 'Is this working?' , 'https://kentdenver.instructure.com/images/thumbnails/15304/ckhEcjfPPMuJeUIXGPMdg7QoyNC0c6Ptd5vdGKfe' , comments , 1)
         posts = [p]
+        print(comments[0].name)
         return render.index(posts)
+    #sees if a comment or post was submitted and handles it
     def POST(self):
         form = web.input()
         try:
@@ -46,8 +53,10 @@ class index:
             comment = form.comment
             print(comment)
             print(myid)
+
         raise web.seeother('/')
 
 #main method
 if __name__ == '__main__':
+
     app.run()

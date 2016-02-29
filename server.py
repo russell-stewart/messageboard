d2de986c7207c4d102ee147ce2b5bd9b5efca68f
russell-stewart <rstewart16@kentdenver.org>
2016-02-29 14:04:24 -0700

    Putting posts in the database kinda works

--- server.py
+++ server.py
@@ -3,9 +3,8 @@ import web
 import pymongo#note: you must first run this terminal command: sudo python -m pip install pymongo to use this package.
 #Database setup: Create a database named messageboard. Add a collection called posts.
 from pymongo import MongoClient
-client = MongoClient('localhost' , 27017)
-db = client.messageboard
-collection = db.posts
+client = MongoClient('mongodb://localhost:27017')
+db = client['Messageboard']
 
 
 render = web.template.render('website/')
@@ -40,16 +39,24 @@ class index:
         comments = [c]
         p = Post('Russell' , 'Is this working?' , 'https://kentdenver.instructure.com/images/thumbnails/15304/ckhEcjfPPMuJeUIXGPMdg7QoyNC0c6Ptd5vdGKfe' , comments , 1)
         posts = [p]
-        print(comments[0].name)
         return render.index(posts)
     #sees if a comment or post was submitted and handles it
     def POST(self):
         form = web.input()
+        print('hi')
         try:
             post = form.writepost
+            posts = db.posts.find().sort('myid' , pymongo.DESCENDING)
+            myid = posts[0].get('myid') + 1
+
+            db.posts.insert_one({
+                 'name' : 'should go here',
+                 'text' : post,
+                 'pic' : 'should go here',
+                 'myid' : myid})
             print(post)
         except AttributeError:
-            myid = form.id
+            myid = form.myid
             comment = form.comment
             print(comment)
             print(myid)
@@ -58,5 +65,16 @@ class index:
 
 #main method
 if __name__ == '__main__':
-
+    c = Comment('Bob' , 'This should be a comment.' , 1)
+    comments = [c]
+    # p = Post('Russell' , 'Is this working?' , 'https://kentdenver.instructure.com/images/thumbnails/15304/ckhEcjfPPMuJeUIXGPMdg7QoyNC0c6Ptd5vdGKfe' , comments , 1)
+    # db.posts.insert_one({
+    #     'name' : p.name,
+    #     'text' : p.text,
+    #     'pic' : p.pic,
+    #     'myid' : p.myid})
+    # db.comments.insert_one({
+    #     'name' : c.name,
+    #     'text' : c.text,
+    #     'referenceid' : c.referenceid})
     app.run()
